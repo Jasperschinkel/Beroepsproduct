1b2632fba77eed21497d1bab35825cbc5462a36b
18064728 <47145360+18064728@users.noreply.github.com>
2019-03-31 14:18:58 +0200

    Update Grass.java

--- Grass.java
+++ Grass.java
@@ -1,16 +1,23 @@
-import javax.swing.*;
 import java.awt.*;
+import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 public class Grass extends JComponent {
-    private Color color;
+    private  Image grass;
 
     public Grass() {
-        this.color = Color.GREEN;
+        try {
+            grass = ImageIO.read(new File("C:\\Users\\Joost\\IdeaProjects\\TestField2\\src\\tiles\\grass.png"));
+        } catch (IOException e) {
+            System.out.println("error");
+        }
     }
 
     @Override
-    protected void paintComponent(Graphics looks) {
-        looks.setColor(color);
-        looks.fillRect(0,0,35,35);
+    protected void paintComponent(Graphics g) {
+       g.drawImage(grass,0,0, this);
+
     }
 }
